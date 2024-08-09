1c66b647b708e5d4cb1a87b6d3ba44bf4740bafa
lucky99ethan <ethansawe99@gmail.com>
2024-08-09 16:24:40 +0300

    initial stage

---
+++ change-page/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="bg-white h-screen w-full flex items-center justify-center font-roboto">
+      <div className="flex justify-between w-full px-20">
+        <div>
+          <h1 className="text-black text-center">Finding your dream home</h1>
+        </div>
+        <div>
+          <h1 className="text-black text-center">Register your details</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
